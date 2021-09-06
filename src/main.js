8195d98916988adfe279b70ab028b63687b59d61
xingHome <xingqq2233@qq.com>
2021-09-06 13:12:36 +0800

    优化样式文件

--- src/main.js
+++ src/main.js
@@ -12,11 +12,11 @@ import '@/assets/icon/iconfont.css'
 // <!-- <link href="https://cdn.bootcss.com/github-markdown-css/2.8.0/github-markdown.min.css" rel="stylesheet">
 // <link href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/github-gist.min.css" rel="stylesheet"> -->
 
-import '@/assets/css/github-gist.min.css'
-import '@/assets/css/github-markdown.min.css'
+// import '@/assets/css/github-gist.min.css'
+// import '@/assets/css/github-markdown.min.css'
 
 import '@/assets/css/main.less'
-import '@/assets/vs2015.css'
+// import '@/assets/vs2015.css'
 // 指定请求的根路径
 // axios.defaults.baseURL = 'http://192.168.31.189'
 axios.defaults.withCredentials = true // axios请求携带cookie    每个都需要配置
