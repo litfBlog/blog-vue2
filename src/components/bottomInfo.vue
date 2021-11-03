ce90c1b5634d1f69855d45150c5f315ede892b4a
XING2233 <xingqq2233@qq.com>
2021-11-03 14:36:39 +0800

    修改备案号，换域名

--- src/components/bottomInfo.vue
+++ src/components/bottomInfo.vue
@@ -47,19 +47,12 @@
       <div class="copyRighr">&copy;2021 litfa all rights reserved</div>
       <div class="icp">
         <a href="https://beian.miit.gov.cn">
-          豫ICP备2021024510号
+          豫ICP备2021024510号-1
         </a>
-
-        <!-- <div style="width:300px;margin:0 auto; padding:20px 0;">
-          <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41018202000798" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-            <img :src="beiAnTuPian" style="float:left;" />
-            <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">豫公网安备 41018202000798号</p>
-          </a>
-        </div> -->
         <!-- <div style="width:300px;margin:0 auto; padding:20px 0;display: inline-block;"> -->
-        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41018202000798" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=4101820200807" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
           <img :src="beiAnTuPian" />
-          <span style="height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">豫公网安备 41018202000798号</span>
+          <span style="height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">豫公网安备 4101820200807号</span>
         </a>
         <!-- </div> -->
 
